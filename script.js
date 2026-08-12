a743904ff120252a959dc73bc6cba3c6c7d669b3
akindelet20-maker <akindelet20@gmail.com>
2026-08-12 22:04:51 +0100

    Fix duplicate certificate ID declaration

--- script.js
+++ script.js
@@ -1977,9 +1977,6 @@ if (!studentName) {
     );
 }
 
-const certificateId =
-    "SIF-FX-" + new Date().getFullYear() + "-" +
-    Math.floor(1000 + Math.random() * 9000);
 
 const certificateId =
     "SIF-FX-" +
